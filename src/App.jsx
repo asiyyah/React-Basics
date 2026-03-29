ea08ceb39f5fa98d6f4a0c1bc3fa8d7e5068f289
ashiatabdulkadir <ashiatabdulkadir@gmail.com>
2026-03-29 17:32:43 +0100

    rendering-examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 // import Counter from "./Counter.jsx"
 // import InputComponent from "./InputComponent.jsx"
-import ShoppingList from "./ShoppingList.jsx"
-import Button from "./Button.jsx"
+// import ShoppingList from "./ShoppingList.jsx"
+// import Button from "./Button.jsx"
 import Conditional from "./Conditional.jsx"
 function App(){
   // A component can have dynamic data which is simply JavaScript variables
   return(
     <>
- {/* <Counter />
- <InputComponent /> */}
- <ShoppingList />
  <Conditional />
- {/* <Button /> */}
     </>
   )
 }
--- src/Conditional.jsx
+++ src/Conditional.jsx
@@ -1,18 +1,52 @@
-function Conditiona(){
-const isLoggedIn = true
-if(isLoggedIn){
-    return(
+function Conditional(){
+// const isLoggedIn = true
+
+// // Using if/else statement
+// // if(isLoggedIn){
+// //     return(
+// //         <>
+// //         <h1>Welcome to the Dashboard</h1>
+// //         </>
+// //     )
+// // }
+// // else{
+// //     return(
+// //         <>
+// //         <h1>Please Login to Continue</h1>
+// //         </>
+// //     )   
+// // }
+
+// // Using ternary operator
+// return(
+//     <>
+//     <h1>{isLoggedIn? "Welcome to App" : "Please Login to Continue"}</h1>
+
+//     {/* {isLoggedIn? <h1>Welcome to the Dashboard</h1> : <h1>Please Login to Continue</h1>} */}
+//     </>
+// )
+
+const hasPaid = true
+
+return(
+    <>
+    <h1>Dev and Design Dashboard</h1>
+    <p>DESIGN TO MVP BOOTCAMP</p>
+
+    {hasPaid ? (
         <>
-        <h1>Welcome to the Dashboard</h1>
+        <h3>Access Granted</h3>
+        <p>Welcome to class</p>
+        <p>Access live class link Here</p>
+        </>
+    ) : (
+        <>
+        <h3>Payment Required for Access</h3>
+        <p>Please make payment to access the course content</p>
         </>
     )
-}
-else{
-    return(
-        <>
-        <h1>Please Login to Continue</h1>
-        </>
-    )   
-}
+    }
+    </>
+)
 }
 export default Conditional
