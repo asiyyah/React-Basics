34237aeea48b52cbd79747f717c3335b36a06872
ashiatabdulkadir <ashiatabdulkadir@gmail.com>
2026-03-29 14:53:58 +0100

    shopping-list

--- src/ShoppingList.jsx
+++ src/ShoppingList.jsx
@@ -1,5 +1,22 @@
 function ShoppingList(){
-    
+    const shoppingList = [
+        "Shoes",
+        "Bags",
+        "Shorts",
+        "T-shirts",
+        "Sneakers"
+    ]
+
+    return(
+        <>
+        <h1>Shopping List</h1>
+        <ul>
+            {shoppingList.map((item, index) => (
+                <li key={index}>{item}</li>
+            ))}
+        </ul>
+        </>
+    )
 }
 
 export default ShoppingList
