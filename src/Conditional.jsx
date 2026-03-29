0ba6123263df8d1f48740153d8273ef2936dbeaa
ashiatabdulkadir <ashiatabdulkadir@gmail.com>
2026-03-29 17:48:18 +0100

    result-example

--- src/Conditional.jsx
+++ src/Conditional.jsx
@@ -26,27 +26,49 @@ function Conditional(){
 //     </>
 // )
 
-const hasPaid = true
+// const hasPaid = true
 
-return(
+// return(
+//     <>
+//     <h1>Dev and Design Dashboard</h1>
+//     <p>DESIGN TO MVP BOOTCAMP</p>
+
+//     {hasPaid ? (
+//         <>
+//         <h3>Access Granted</h3>
+//         <p>Welcome to class</p>
+//         <p>Access live class link Here</p>
+//         </>
+//     ) : (
+//         <>
+//         <h3>Payment Required for Access</h3>
+//         <p>Please make payment to access the course content</p>
+//         </>
+//     )
+//     }
+//     </>
+// )
+
+const resultAvailable = true
+const studentName = `Rose`
+const score =250
+
+return (
+     <>
+<h1>Exam Result Portal</h1>
+<h3>Hello: {studentName}</h3>
+
+{resultAvailable ? (
     <>
-    <h1>Dev and Design Dashboard</h1>
-    <p>DESIGN TO MVP BOOTCAMP</p>
-
-    {hasPaid ? (
-        <>
-        <h3>Access Granted</h3>
-        <p>Welcome to class</p>
-        <p>Access live class link Here</p>
-        </>
-    ) : (
-        <>
-        <h3>Payment Required for Access</h3>
-        <p>Please make payment to access the course content</p>
-        </>
-    )
-    }
+    <h3>Your result is available</h3>
+    <p>Your score is: {score}</p>
     </>
+) : (
+    <>
+    <h3>Result Not Available</h3>
+    </>
+)}
+</>
 )
 }
 export default Conditional
