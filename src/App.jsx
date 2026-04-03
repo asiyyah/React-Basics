b06ee57b2e8a7fc56eb4c3b8c7905b0f2470ee61
ashiatabdulkadir <ashiatabdulkadir@gmail.com>
2026-04-03 13:06:08 +0100

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 import Home from "./Home"
 import About from "./About"
 import Contact from "./Contact" 
-import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
+
 function App(){
   // A component can have dynamic data which is simply JavaScript variables
   return(
     <>
-    <BrowserRouter>
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
-    </nav>
-
-     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-    </BrowserRouter>
+    
 
    
     
---
+++ src/Posts.jsx
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from "react"
+
+function Posts(){
+    const [posts, setPosts] = useState([])
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.json())
+        .then(data => setPosts(data))
+    }, [])
+    return(
+        <>
+        <h1>Posts</h1>
+        <ul>
+            {posts.map(post => (
+                <li key={post.id}>{post.title}</li>
+            ))}
+        </ul>
+        </>
+    )
+}
+export default Posts
